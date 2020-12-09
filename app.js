4c655d1ff8c06945b0b52a70b606c69ed6257acc
Abhay <ap3919529@gmail.com>
2020-12-10 00:51:58 +0530

    output value is given

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ var btnTranslate = document.querySelector("#btn-translate")
 
 var txtInput = document.querySelector("#txt-input");
 
+
+var outDiv = document.querySelector("#output");
+
+// outDiv.innerText = "Abhay patil";
+
 console.log(txtInput);
 
 function clickHandler(){
     console.log("clicked");
+    console.log("input",txtInput.value);
+    console.log("output", outDiv.value);
 };
 
 btnTranslate.addEventListener("click", clickHandler)
